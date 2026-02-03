2c81c9843907b8c25320a1f4f82cdd84fe1de716
bgltrk <rnbxwzwprz@privaterelay.appleid.com>
2026-02-03 17:05:00 +0300

    Add Dockerfile for deploment

---
+++ web/spotifyoutfit/SpotifyOutfitWebApplication.java
@@ -0,0 +1,13 @@
+package com.spotifyoutfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyOutfitWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyOutfitWebApplication.class, args);
+	}
+
+}
